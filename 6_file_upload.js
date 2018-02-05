ffdf2d44c7e761726194da00b5dfd3191c995828
Akilan S <Akilan_S@ad.infosys.com>
2018-02-05 19:50:35 +0530

    File Upload example added

--- 6_file_upload.js
+++ 6_file_upload.js
@@ -1,10 +1,28 @@
-const file_uploder = require("formidable");
+const file_uploder = require('formidable');
 const http = require('http');
 const fs = require("fs");
 
 http.createServer((req,res)=>{
     if(req.url == "/upload"){
-        const form = new formidable.IncomingForm();
+        var form = new file_uploder.IncomingForm();
+        form.parse(req, function (err, fields, files) {
+           console.log(fields);
+           console.log(files);
+
+           const old_path = files.profile_picture.path;
+           const new_path = files.profile_picture.name;
+
+            fs.rename(old_path,new_path,(error)=>{
+                if(error){
+                    throw error;
+                }else{
+                    res.writeHead(200,{'Content-Type':'text/html'});
+                    res.write("File Uploded successfully");
+                    res.end();
+                }
+            });
+
+        })
     }else{
         fs.readFile("form.html",(err,data)=>{
             if(err){
