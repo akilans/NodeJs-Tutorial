73b8cb00af4cdfa50663ede0d84079e17060a87f
Akilan <akil.dove@gmail.com>
2018-02-06 08:45:40 +0530

    Insert many code added

--- 9_mdb_insert_data.js
+++ 9_mdb_insert_data.js
@@ -7,13 +7,27 @@ mongodb.connect(db_url, (err, db) => {
 
     const dbo = db.db("employee");
 
-    const newEmplpoyee = { first_name: "Akilan", last_name: "Subramanian", location: "Bangalore" }
+    const newEmplpoyee = { first_name: "Akilan", last_name: "Subramanian", location: "Bangalore" };
 
     dbo.collection("employee").insertOne(newEmplpoyee, (err,res) => {
 
         if(err) throw err;
 
         console.log("Data inserted!!!");
+        db.close();
+
+    });
+
+    const newEmplpoyees = [
+        { first_name: "Mathan", last_name: "Raj", location: "Bangalore" },
+        { first_name: "Selvam", last_name: "Ras", location: "Bangalore" }
+    ];
+
+    dbo.collection("employee").insertMany(newEmplpoyees, (err,res) => {
+
+        if(err) throw err;
+
+        console.log("Total number of Data inserted!!!"+res.insertedCount);
         db.close();
 
     });
